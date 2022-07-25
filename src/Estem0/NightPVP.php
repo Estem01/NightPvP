97a9130a3a92f83fcf6996126a77b9c5f373dfb7
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-07-25 16:55:47 -0300

    Update NightPVP.php

--- src/Estem0/NightPVP.php
+++ src/Estem0/NightPVP.php
@@ -1,1 +1,41 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Estem0;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+class NightPVP extends PluginBase implements Listener{
+  
+    public function onEnable() : void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    public function onEntityDamageByEntity(EntityDamageEvent $event){
+      
+        if($event instanceof EntityDamageByEntityEvent){
+          
+            if(!$this->isNight($event->getEntity()->getLevel()->getTime())){
+              
+                if($event->getEntity() instanceof Player && $event->getDamager() instanceof Player){
+                  
+                    if(!$event->getDamager()->hasPermission("nightpvp.exempt.victim") && !$event->getDamager()->hasPermission("nightpvp.exempt.damager")){
+                        $event->setCancelled();
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    public function isNight($t){
+        return ($t >= 10900 && $t < 17800);
+    }
+}
