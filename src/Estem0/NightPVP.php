369071d0f30824832d2da6a78ca6c994a7b26a6d
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-07-26 11:00:46 -0300

    update NightPVP.php

--- src/Estem0/NightPVP.php
+++ src/Estem0/NightPVP.php
@@ -18,7 +18,7 @@ class NightPVP extends PluginBase implements Listener{
 
     public function onEntityDamageByEntity(EntityDamageEvent $event){
 	
-      		$NPWorld = $event->getOrigin()->getWorld()->getFolderName();
+      		$NPWorld = $event->getPlayer()->getWorld()->getFolderName();
 		      $ANPWorld = (array)$this->getConfig()->get("worlds");
         if($event instanceof EntityDamageByEntityEvent){          
             if(!$this->isNight($event->getEntity()->getWorld()->getTime())){
@@ -36,7 +36,7 @@ class NightPVP extends PluginBase implements Listener{
     }
     
     public function isNight($t){
-		$NPWorld = getOrigin()->$event->getWorld()->getFolderName();
+		$NPWorld = getPlayer()->$event->getWorld()->getFolderName();
 		$ANPWorld = (array)$this->getConfig()->get("worlds");
 		if (!in_array($NPWorld, $ANPWorld)) return;
 		$event->cancel();
