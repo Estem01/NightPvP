551d0e6a492bfdf9dc4c1c121f63cb7f124c35c4
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-08-06 05:37:18 -0300

    Update NightPVP.php

--- src/Estem0/NightPVP.php
+++ src/Estem0/NightPVP.php
@@ -14,13 +14,12 @@ use pocketmine\utils\Config;
 
 class NightPVP extends PluginBase implements Listener{
     
-    private Config $config;
+    private Config[$config];
 
     public function onEnable() : void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveResource("config.yml");
         $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-        $this->getConfig("config.yml");
     }
 
     public function onEntityDamageByEntity(EntityDamageEvent $event) {
