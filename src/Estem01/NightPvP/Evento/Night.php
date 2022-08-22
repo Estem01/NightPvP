85a92a1e38c4aa55305e3f8ea3ca735f3b540f91
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-08-22 15:52:03 -0300

    Create Night.php

---
+++ src/Estem01/NightPvP/Evento/Night.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace Estem01\NightPvP\Event;
+
+use Estem01\NightPvP\Loader;
+use Estem01\NightPvP\EventListener;
+
+class Night {
+  
+  public function isNight(int $t) : bool
+
+    {
+
+        if ($t >= 13000 && $t < 18000) {
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+}
