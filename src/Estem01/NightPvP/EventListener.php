02f24b26427a7868a6278bd05834da2c91453889
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-08-27 04:31:15 -0500

    Ok

--- src/Estem01/NightPvP/EventListener.php
+++ src/Estem01/NightPvP/EventListener.php
@@ -14,7 +14,7 @@ use pocketmine\world\WorldManager;
 
 class EventListener implements Listener {
 
-    public function onEntityDamageByEntity(EntityDamageByEntityEvent $event): void{
+    public function onDamageEntity(EntityDamageByEntityEvent $event): void{
         $entity = $event->getEntity();
         $damager = $event->getDamager();
         if($entity instanceof Player and $damager instanceof Player) {
