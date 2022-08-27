a6692e394475cdaa60bb5f0317b690533a3d2565
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-08-27 04:19:04 -0500

    Hmm

--- src/Estem01/NightPvP/Event/Night.php
+++ src/Estem01/NightPvP/Event/Night.php
@@ -7,28 +7,13 @@ use Estem01\NightPvP\EventListener;
 use pocketmine\world\World;
 
 class Night {
-
-  public function __construct(private Main $main) {
-        $this->main = $main;
-    }
-  
-  public function isNight(int $t) : bool
-
-    {
-        
-    $t = getTime();
     
-        if ($t >= 13000 && $t < 18000) {
-
-        return true;
-
+    public function isNight(int $t): bool{    
+    $t = getTime();
+        if($t >= 13000 && $t < 18000){
+          return true;
         }else{
-
-        return false;
-      }
-    }
- 
- public function getMain() : Main {
-        return $this->main;
+          return false;
+        }
     }
 }    
