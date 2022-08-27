c13ef68c81eb9dc9a7084e34f0d3490453a43c4d
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-08-27 04:13:08 -0500

    Ok

--- src/Estem01/NightPvP/Main.php
+++ src/Estem01/NightPvP/Main.php
@@ -6,28 +6,27 @@ namespace Estem01\NightPvP;
 
 use Estem01\NightPvP\Event\Night;
 use Estem01\NightPvP\EventListener;
-use pocketmine\event\Listener;
+
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase{
 
-  private static Main $instance;
+    public static Main $instance;
     public Config $config;	
 
-    public function onEnable() : void {
-    
+    public function onEnable(): void{
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->getServer()->getPluginManager()->registerEvents(new Night($this), $this);
         $this->saveResource("config.yml");
         $this->config = new Config($this->getDataFolder() . "config.yml");
     }
 
- public function onLoad() : void {
+    public function onLoad(): void{
         self::$instance = $this;
     }
 	
-    public static function getInstance() : Main {
+    public static function getInstance(): Main{
         return self::$instance;
     }
 }
