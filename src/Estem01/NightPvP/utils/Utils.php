eb479782eee5ed4ac5aec3166920c5cb4bae8736
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-08-27 00:15:33 -0300

    Create Utils.php

---
+++ src/Estem01/NightPvP/utils/Utils.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Estem01\NigtPvP\Utils;
+
+use pocketmine\player\Player;
+
+use pocketmine\network\mcpe\protocol\PlaySoundPacket;
+
+class Utils {
+
+    public function playSound(Player $player, string $soundName = " ", float $volume = 0, float $pitch = 0) {
+
+        $pk = new PlaySoundPacket();
+
+        $pk->x = $player->getPosition()->getX();
+
+        $pk->x = $player->getPosition()->getY();
+
+        $pk->x = $player->getPosition()->getZ();
+
+        $pk->soundName = $soundName;
+
+        $pk->volume = $volume;
+
+        $pk->pitch = $pitch;
+
+        $player->getNetworkSession()->senddataPacket($pk);
+
+    }
+
+}
