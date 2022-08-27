80cda47ba5cb14fba45abcc201bcd3b804fa47f8
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-08-27 04:09:58 -0500

    Fix test1

--- src/Estem01/NightPvP/EventListener.php
+++ src/Estem01/NightPvP/EventListener.php
@@ -3,7 +3,7 @@
 namespace Estem01\NightPvP;
 
 use Estem01\NightPvP\Main;
-use Estem0\NightPvP\Event\Night;
+use Estem01\NightPvP\Event\Night;
 use pocketmine\player\Player;
 use pocketmine\utils\Config;
 use pocketmine\event\Listener;
@@ -17,24 +17,17 @@ class EventListener implements Listener {
         $this->main = $main;
     }
 
-public function onEntityDamageByEntity(EntityDamageByEntityEvent $event) : void
-
-    {
+public function onEntityDamageByEntity(EntityDamageByEntityEvent $event) : void{
 
         $entity = $event->getEntity();
 
         $damager = $event->getDamager();
 
         if ($entity instanceof Player and $damager instanceof Player) {
-
             if (!$this->isNight($entity->getWorld()->getTime())) {
-
-                if (in_array($entity->getWorld()->getFolderName(), $this->getConfig()->get("worlds"))) {
-
-                  $this->$event->getPlayer()->sendTip("§4PvP Enabled");
-
+                if (in_array($entity->getWorld()->getFolderName(), $this->main->getConfig()->get("worlds"))){
+                  $entity->sendTip("§4PvP Enabled");
                     if (!$damager->hasPermission("nightpvp.exempt.victim") and $damager->hasPermission("nightpvp.exempt.damager")) {
-
                         $event->cancel();
                   }
               }
