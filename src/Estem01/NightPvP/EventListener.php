36361086d6bf2298f717a031da1cf3e8ab70469c
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-10-09 03:13:09 -0300

    Update EventListener.php

--- src/Estem01/NightPvP/EventListener.php
+++ src/Estem01/NightPvP/EventListener.php
@@ -18,20 +18,19 @@ class EventListener implements Listener {
     public function onDamageEntity(EntityDamageByEntityEvent $event): void{
 
         $player = getPlayer();
-         $entity = $event->getEntity();
+         $entity = getEntity();
 
         if($entity instanceof Player && $damager instanceof Player) {
             if(!Main::getInstance()->isNight->isNight($entity->getWorld()->getTime())){
                 if(in_array($entity->getWorld()->getFolderName(), Main::getInstance()->config->get("worlds"))){
 
-                  if(in_array(Main::getInstance()->config->get("title"))){
+                  if(!in_array(Main::getInstance()->config->get("title"))){
                   $config = Main::getInstance()->config->get("title");
                   $player->sendTitle("$config");
-                  }
-
                     Utils::playSound($entity, "random.pop2", 1, 1);
                     if (!$damager->hasPermission("nightpvp.exempt.victim") and $damager->hasPermission("nightpvp.exempt.damager")) {
                         $event->cancel();
+                        }
                     }
                 }
             }
