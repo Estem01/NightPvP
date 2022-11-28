6aaa7e0c9a8fb179cd296531485893a37be19391
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-11-27 23:20:56 -0300

    Bruh

---
+++ src/Estem01/NightPvP/Event/EventListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Estem01\NightPvP;
+
+use Estem01\NightPvP\Main;
+use Estem01\NightPvP\Event\Night;
+use Estem01\NightPvP\Utils\Utils;
+
+use pocketmine\server\Server;
+use pocketmine\player\Player;
+use pocketmine\utils\Config;
+
+use pocketmine\event\Listener;
+use pocketmime\event\entity\DamageByEntityEvent;
+
+class EventListener implements Listener {
+
+    public function onDamageEntity(EntityDamageByEntityEvent $event): void {
+
+        $player = getPlayer();
+         $entity = getEntity();
+
+        if($entity instanceof Player && $damager instanceof Player) {
+            if(!Main::getInstance()->isNight->isNight($entity->getWorld()->getTime())){
+                if(in_array($entity->getWorld()->getFolderName(), Main::getInstance()->config->get("worlds"))){
+
+                  foreach(!in_array(Main::getInstance()->config->get("title"))){
+                  $config = Main::getInstance()->config->get("title");
+                  $player->sendTitle("$config");
+                          }
+
+                    foreach(!$damager->hasPermission("nightpvp.exempt.victim") and $damager->hasPermission("nightpvp.exempt.damager")) {
+                        $event->cancel();
+                    }
+                }
+            }
+        }
+    }
