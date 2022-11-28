6a9b367dbfab74726b8b7a1a8db610536a4a4654
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-11-27 23:23:53 -0300

    Update Night.php

--- src/Estem01/NightPvP/Event/Night.php
+++ src/Estem01/NightPvP/Event/Night.php
@@ -3,15 +3,14 @@
 namespace Estem01\NightPvP\Event;
 
 use Estem01\NightPvP\Main;
-use Estem01\NightPvP\EventListener;
 
 class Night implements Listener {
 
-    public function isNight(int $t): bool{ 
+    public function isNight(int $t, $e): bool{ 
         if($t >= 13000 && $t < 18000){
           return true;
           }else{
-          $event->cancel;
+          $e->cancel;
         }
     }
 }    
